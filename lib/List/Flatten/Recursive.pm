3ffaa2daac83586e2318ad6c7cae5a77f9eaa09f
Ryan C. Thompson <ryan@aeolus>
2010-12-04 11:04:34 -0800

    POD markup fix

--- lib/List/Flatten/Recursive.pm
+++ lib/List/Flatten/Recursive.pm
@@ -40,7 +40,7 @@ that may contain other sublists, and replaces those sublists with
 their contents, recursively, until the list no longer contains any
 sublists.
 
-B<flat> makes a best effort to break circular references (that is,
+C<flat> makes a best effort to break circular references (that is,
 lists that contain references to themselves), and should never enter
 an infinite recursion.
 
@@ -54,7 +54,7 @@ sub flat : Exported {
 
 =method flatten_to_listref
 
-Same as B<flat>, but returns a single reference to the resulting list.
+Same as C<flat>, but returns a single reference to the resulting list.
 
 This method is exported only by request. To use this method, put the following at the top of your program:
 
@@ -85,7 +85,7 @@ __END__
 =head1 DESCRIPTION
 
 If you think of nested lists as a tree structure (an in Lisp, for
-example), then B<flat> basically returns all the leaf nodes from an
+example), then C<flat> basically returns all the leaf nodes from an
 inorder tree traversal, and leaves out the internal nodes (i.e.
 listrefs). If the nested list is a DAG instead of just a tree, it
 should still flatten correctly (based on my own definition of
@@ -103,15 +103,15 @@ the output.
 =head2 Self-referential lists should be flattened by reference
 
 If you are going to flatten a list which may contain references to
-itself, you should pass a reference to that list to B<flat>, or else
+itself, you should pass a reference to that list to C<flat>, or else
 things will not work the way you expect. You will end up with an extra
 trip around the circle before the circular reference is caught.
 
-=head2 B<flat> always returns a list
+=head2 C<flat> always returns a list
 
-Even if you call B<flat> on a single scalar, it will still return a
+Even if you call C<flat> on a single scalar, it will still return a
 list with one element in it. If called in scalar context, it will
-return the length of that list. B<flatten_to_listref> would return a
+return the length of that list. C<flatten_to_listref> would return a
 reference to a list with one element. There is no case where the
 original scalar would be returned directly. This is by design. If you
 think this is wrong, email me and tell me why.
